d80c2b702a7130d671f419610a3909ac0bc0fea6
Ryan Murphy <TheDecoder007@gmail.com>
2022-06-03 20:20:37 -0400

    finished initial setup

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+const { note } = require('../db/db.json')
 
 app.use(express.static('public'));
 
@@ -190,5 +191,7 @@ if (window.location.pathname === '/notes') {
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
   noteText.addEventListener('keyup', handleRenderSaveBtn);
 }
+
+module.exports = router;
 
 getAndRenderNotes();
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 const path = require('path');
+const router = require('express').Router();
 
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { notes } = require('./db/db.json')
+const { note } = require('./db/db.json')
 
 
 //two middleware functions must be set up for server that acccepts POST data
@@ -16,6 +18,34 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+//GET request to get notes from db.json 
+router.get('/api/notes', (req, res) =>{
+    // let results = notes;
+    res.json(notes);
+});
+
+router.get('/api/note', (req, res) =>{
+    let results = note;
+    res.json(results);
+});
+
+//POST to create a new note
+router.post('/api/notes', (req, res) => {
+    //set id for next index in array
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+});
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
 
 
 
